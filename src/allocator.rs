2720f7233c0a263fd2cade059e10c769c9975b77
Ryan Frazier <rfrazier716@gmail.com>
2023-02-05 22:08:55 -0500

    initial commit. wrote an allocator so we can have references to items easily.

---
+++ src/allocator.rs
@@ -0,0 +1,180 @@
+mod allocator {
+    use std::{collections::VecDeque, fmt::Error};
+
+    type AllocatorGeneration = u64;
+
+    #[derive(Debug, Copy, Clone, PartialEq)]
+    struct Key {
+        index: usize,
+        generation: AllocatorGeneration,
+    }
+
+    enum AllocatorSlot<T> {
+        Free(AllocatorGeneration),
+        Occupied(AllocatorGeneration, T),
+    }
+
+    struct Allocator<T> {
+        elements: Vec<AllocatorSlot<T>>, // the stored data managed by the allocator
+        free_slots: VecDeque<usize>,     // list of the open slots we can insert into
+    }
+
+    impl<T> Allocator<T> {
+        fn new() -> Self {
+            Allocator {
+                elements: Vec::new(),
+                free_slots: VecDeque::new(),
+            }
+        }
+
+        fn key_is_valid(&self, key: &Key) -> bool {
+            // checks if a key is
+            if let Some(value) = self.elements.get(key.index) {
+                match value {
+                    AllocatorSlot::Occupied(generation, _) => *generation == key.generation,
+                    AllocatorSlot::Free(_) => false,
+                }
+            } else {
+                false
+            }
+        }
+        fn insert(&mut self, value: T) -> Key {
+            if let Some(next_free_index) = self.free_slots.pop_front() {
+                // if we have a free index, reuse it
+                match self.elements[next_free_index] {
+                    AllocatorSlot::Free(generation) => {
+                        self.elements[next_free_index] = AllocatorSlot::Occupied(generation, value);
+                        Key {
+                            index: next_free_index,
+                            generation,
+                        }
+                    }
+                    _ => panic!("Tried to insert into a non-empty slot"),
+                }
+            } else {
+                // otherwise extend the vector to hold the new element
+                self.elements.push(AllocatorSlot::Occupied(0, value));
+                Key {
+                    index: self.elements.len() - 1,
+                    generation: 0,
+                }
+            }
+        }
+
+        fn get(&self, key: &Key) -> Option<&T> {
+            if self.key_is_valid(key) {
+                if let AllocatorSlot::Occupied(_, val) = &self.elements[key.index] {
+                    Some(val)
+                } else {
+                    // This should be unreachable based on our valid key conditions
+                    None
+                }
+            } else {
+                None
+            }
+        }
+
+        fn get_mut(&mut self, key: &Key) -> Option<&mut T> {
+            if self.key_is_valid(key) {
+                if let AllocatorSlot::Occupied(_, val) = &mut self.elements[key.index] {
+                    Some(val)
+                } else {
+                    // This should be unreachable based on our valid key conditions
+                    None
+                }
+            } else {
+                None
+            }
+        }
+
+        fn free(&mut self, key: Key) -> Result<(), Key> {
+            // this will consume the key and remove it from the allocator
+            // If it cannot remove the  index, the key is returned as an error
+            // TODO: Find a better way to clean this up - returning Err(key) is odd
+            if self.key_is_valid(&key) {
+                self.elements[key.index] = AllocatorSlot::Free(key.generation + 1);
+                self.free_slots.push_back(key.index);// put the slot into our free slot pool
+                Ok(())
+            } else {
+                Err(key)
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_allocator_insertion() {
+            // creating a new allocator and allocating a single cell returns the zero index
+            let mut allocator = Allocator::new();
+            for i in 0..3 {
+                assert_eq!(
+                    allocator.insert(2 * i),
+                    Key {
+                        index: i,
+                        generation: 0
+                    }
+                )
+            }
+        }
+
+        #[test]
+        fn test_allocator_getter() {
+            let mut allocator = Allocator::new();
+            let key = allocator.insert(4.5);
+
+            // assert a valid key works
+            assert_eq!(allocator.get(&key), Some(&4.5));
+
+            // an invalid generation returns None
+            assert_eq!(
+                allocator.get(&Key {
+                    index: 0,
+                    generation: 3
+                }),
+                None
+            );
+
+            // assert an invalid length returns none
+            assert_eq!(
+                allocator.get(&Key {
+                    index: 100,
+                    generation: 0
+                }),
+                None
+            );
+        }
+
+        #[test]
+        fn test_mutable_get() {
+            let original_value = 4.5;
+            let new_value = 47.5;
+            let mut allocator = Allocator::new();
+            let key = allocator.insert(original_value);
+
+            if let Some(val) = allocator.get_mut(&key) {
+                *val = new_value;
+            }
+
+            assert_eq!(allocator.get(&key), Some(&new_value));
+        }
+
+        #[test]
+        fn test_slot_reuse() {
+            let original_value = 4.5;
+            let new_value = 47.5;
+            let mut allocator = Allocator::new();
+            let key = allocator.insert(original_value);
+            allocator.free(key).unwrap(); // free the key so we can reuse the slot
+            let key = allocator.insert(new_value);
+
+            // assert the new value is stored
+            assert_eq!(allocator.get(&key), Some(&new_value));
+
+            // assert that the key has increased its generation by one
+            assert_eq!(key, Key{generation: 1, index: 0});
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,134 @@
+mod allocator;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl Point2D {
+    fn new(x: f64, y: f64) -> Self {
+        Point2D { x, y }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum SegmentOrientation {
+    Left,
+    Right,
+    Colinear,
+}
+
+fn orientation(point1: &Point2D, point2: &Point2D, point3: &Point2D) -> SegmentOrientation {
+    // If we view the three points as A, B, and C, and Lines AB and BC,
+    // If the z- component of the cross product of ABxBC is positive, it's left oriented
+    // if it's negative it's right oriented
+    let cross_product = (point2.x - point1.x) * (point3.y - point2.y)
+        - (point2.y - point1.y) * (point3.x - point2.x);
+    if cross_product > 0.0 {
+        SegmentOrientation::Left
+    } else if cross_product < 0.0 {
+        SegmentOrientation::Right
+    } else {
+        SegmentOrientation::Colinear
+    }
+}
+
+fn convex_hull(points: &Vec<Point2D>) -> Option<Vec<Point2D>> {
+    if points.len() < 3 {
+        return None;
+    }
+
+    // lexically sort our points from left to right, bottom to top
+    let mut sorted_points = points.to_vec();
+    sorted_points.sort_by(|a, b| match a.x.total_cmp(&b.x) {
+        core::cmp::Ordering::Equal => a.y.total_cmp(&b.y),
+        cmp => cmp,
+    });
+    println!("sorted: {:?}", sorted_points);
+    let mut upper_hull = Vec::new();
+
+    for point in &sorted_points {
+        if upper_hull.len() < 2 {
+            // don't have enough points yet, push back the buffer
+            upper_hull.push(*point)
+        } else {
+            println!("point: {:?}", point);
+            while orientation(&upper_hull[upper_hull.len()-2], &upper_hull.last().unwrap(), point) == SegmentOrientation::Left {
+                upper_hull.pop(); // pop the offending element
+            }
+            upper_hull.push(*point);
+        }
+    }
+    let upper_hull_points = upper_hull.len();
+    for point in sorted_points.iter().rev().skip(1) {
+        if upper_hull.len() < upper_hull_points + 1 {
+            // don't have enough points yet, push back the buffer
+            upper_hull.push(*point)
+        } else {
+            println!("point: {:?}", point);
+            while orientation(&upper_hull[upper_hull.len()-2], &upper_hull.last().unwrap(), &point) == SegmentOrientation::Left {
+                upper_hull.pop(); // pop the offending element
+            }
+            upper_hull.push(*point);
+        }
+    }
+    Some(upper_hull)
+}
+
+fn main() {
+    let points = vec![
+        Point2D::new(0.0, 3.0),
+        Point2D::new(2.0, 3.0),
+        Point2D::new(1.0, 2.0),
+        Point2D::new(3.0, 2.0),
+        Point2D::new(4.0, 2.0),
+        Point2D::new(2.0, 1.0),
+        Point2D::new(3.0, 1.0),
+        Point2D::new(0.0, 0.0),
+        Point2D::new(2.0, 0.0),
+        Point2D::new(4.0, 0.0),
+        Point2D::new(1.0, -1.0),
+        Point2D::new(5.0, -1.0),
+        Point2D::new(3.0, -2.0),
+    ];
+    println!("{:?}", convex_hull(&points));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_orientation() {
+        let test_table = vec![
+            (
+                [
+                    Point2D::new(0.0, 0.0),
+                    Point2D::new(1.0, 1.0),
+                    Point2D::new(2.0, 2.0),
+                ],
+                SegmentOrientation::Colinear,
+            ),
+            (
+                [
+                    Point2D::new(0.0, 0.0),
+                    Point2D::new(1.0, 1.0),
+                    Point2D::new(2.0, 1.0),
+                ],
+                SegmentOrientation::Right,
+            ),
+            (
+                [
+                    Point2D::new(0.0, 0.0),
+                    Point2D::new(1.0, 1.0),
+                    Point2D::new(1.0, 2.0),
+                ],
+                SegmentOrientation::Left,
+            ),
+        ];
+        for (points, expected) in test_table {
+            assert_eq!(expected, orientation(&points[0], &points[1], &points[2]));
+        }
+    }
+}
